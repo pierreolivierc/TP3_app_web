1d7e6ba86d61573e1e5ec07e97551fba14c5cbef
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-05-01 08:36:18 -0400

    controller connexion

--- tp3-h24-main/api/controllers/userController.js
+++ tp3-h24-main/api/controllers/userController.js
@@ -9,11 +9,38 @@ dotenv.config();
 const url_base = process.env.URL + ":" + process.env.PORT;
 
 exports.createUser = (req, res, next) => {
-			
+    const {username, email, password, passwordConfirmation} = req.body
+
+    const user = new User({
+        username: username,
+        email: email,
+        password: password
+    })
+    user.save()
+        .then(result => {
+            res.location("/users/" + result._id)
+            // Retourne le résultat au format JSON
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            next(err)
+        })
 };
 
 exports.getUser = (req, res, next) => {
-	
+const userId = req.params.email
+  User.findById(userId)
+  .then(user => {
+    if (!user) {
+      const error = new Error('L\'utilisateur n\'existe pas.')
+      error.statusCode = 404
+      throw error
+    }
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    next(err)
+  })
 };
 
 
