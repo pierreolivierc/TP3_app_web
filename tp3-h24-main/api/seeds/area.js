971a5ef203b37812579c9bd3cc18b0ad512a072b
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-05-03 11:22:22 -0400

    seed

---
+++ tp3-h24-main/api/seeds/area.js
@@ -0,0 +1,39 @@
+const area = [
+    {
+        "name": "Falaise du Pic Vert",
+        "description": "Falaise située au sommet d'une colline verdoyante, offrant une vue panoramique imprenable. Les voies sont principalement orientées à l'ouest, parfaites pour grimper au coucher du soleil.",
+        "gettingThere": "Pour accéder à la falaise, suivre le sentier balisé à partir du parking situé à la lisière de la forêt. Environ 20 minutes de marche.",
+        "lon": 1.234567,
+        "lat": 2.345678,
+        "routes": [],
+        "user": "user1@gmail.com"
+    },
+    {
+        "name": "Grotte des Chimères",
+        "description": "Une grotte cachée au fond d'une vallée mystérieuse, renfermant des voies de difficulté variée. Les parois de la grotte offrent une protection contre les intempéries, idéal pour l'escalade par temps pluvieux.",
+        "gettingThere": "Pour rejoindre la grotte, suivez le sentier escarpé en direction de la vallée. Attention aux rochers glissants par temps humide.",
+        "lon": 3.456789,
+        "lat": 4.567890,
+        "routes": [],
+        "user": "user1@gmail.com"
+    },
+    {
+        "name": "Mur du Dragon",
+        "description": "Un mur de calcaire vertical émergeant au cœur d'une ancienne carrière abandonnée. Les prises sont variées, offrant des défis pour les grimpeurs de tous niveaux.",
+        "gettingThere": "L'accès au mur se fait depuis le parking situé à proximité de l'entrée de la carrière. Suivez le sentier qui serpente à travers les arbres jusqu'au pied du mur.",
+        "lon": 5.678901,
+        "lat": 6.789012,
+        "routes": [],
+        "user": "user2@gmail.com"
+    },
+    {
+        "name": "Rocher du Griffon",
+        "description": "Un rocher isolé perché au sommet d'une crête escarpée. Les voies sont techniques et exposées, réservées aux grimpeurs expérimentés en quête de sensations fortes.",
+        "gettingThere": "La montée jusqu'au rocher nécessite une courte mais intense ascension depuis le sentier de montagne. Attention au vent fort sur la crête.",
+        "lon": 7.890123,
+        "lat": 8.901234,
+        "routes": [],
+        "user": "user2@gmail.com"
+    }
+]
+module.exports = area;
---
+++ tp3-h24-main/api/seeds/user.js
@@ -0,0 +1,14 @@
+const user = [
+    {
+        "username": "User1",
+        "email": "user1@gmail.com",
+        "password": "MotDePasse1"
+    },
+    {
+        "username": "User2",
+        "email": "user2@gmail.com",
+        "password": "MotDePasse2"
+    }
+]
+
+module.exports = user;
