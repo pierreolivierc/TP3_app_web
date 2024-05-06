1ecf49e62e4d7798c45976f82477d79e76ffa6c6
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2024-05-06 17:03:18 -0400

    correction profil

--- tp3-h24-main/DrTopo/src/components/NavBar.vue
+++ tp3-h24-main/DrTopo/src/components/NavBar.vue
@@ -22,15 +22,16 @@
           </router-link>
         </li>
         <li>
-          <router-link v-on:click="logout" v-show="isLogged" to="/" class="nav-link me-3 py-2 ">
-            Deconnexion
-          </router-link>
-        </li>
-        <li>
           <router-link v-show="isLogged"  to="/profile" class="nav-link me-3 py-2" :class="{ 'active': $route.path === '/profile' }">
             Profil
           </router-link>
         </li>
+        <li>
+          <router-link v-on:click="logout" v-show="isLogged" to="/" class="nav-link me-3 py-2 ">
+            Deconnexion
+          </router-link>
+        </li>
+
       </ul>
     </nav>
   </div>
@@ -39,24 +40,40 @@
 
 <script>
 import 'bootstrap/dist/css/bootstrap.css';
+// import { jwtDecode  } from 'jwt-decode';
+
 export default {
   data() {
     return {
-      isLogged: false
+      isLogged: false,
+      user: {}
     }
   },
   watch: {
     '$route' () {
-      this.isLogged = localStorage.getItem('token') !== null
+      this.isLogged = localStorage.getItem('jwt') !== null
+      // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+      // if (token) {
+      //   const decoded = jwtDecode(token);
+      //   this.user = decoded.user;
+      // } else {
+      //   this.user = {};
+      // }
     }
   },
   created() {
-    this.isLogged = localStorage.getItem('token') !== null
+    this.isLogged = localStorage.getItem('jwt') !== null
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+    // if (token) {
+    //   const decoded = jwtDecode(token);
+    //   this.user = decoded.user;
+    // }
   },
   methods: {
     logout() {
-      localStorage.removeItem('token')
+      localStorage.removeItem('jwt')
       this.isLogged = false
+      this.user = {};
       this.$router.push({ name: 'Home' })
     }
   }
--- tp3-h24-main/DrTopo/src/views/ProfileView.vue
+++ tp3-h24-main/DrTopo/src/views/ProfileView.vue
@@ -1,13 +1,53 @@
-<script>
-export default {
-  name: "ProfileView"
-}
-</script>
-
 <template>
-
+  <div class="container mt-5">
+    <h2>Profile</h2>
+    <p>Nom: {{ user.username }}</p>
+    <div class="col-md-12">
+      <h2>Mes lieux <a href="/areas/new">ajouter</a></h2>
+      <table class="table table-striped">
+        <tbody>
+          <tr v-for="(area, index) in areas" :key="index">
+            <td><a :href="'/areas/' + area.id">{{ area.name }}</a></td>
+            <td>{{ area.location }}</td>
+            <td><a href="/areas/new/">modifier</a> <a href="/areas/new/">supprimer</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-md-12">
+      <h2>Mes voies <a href="/routes/new">ajouter</a></h2>
+      <table class="table table-striped">
+        <tbody>
+          <tr v-for="(route, index) in routes" :key="index">
+            <td><a :href="'/routes/' + route.id">{{ route.name }}</a></td>
+            <td>{{ route.area }}</td>
+            <td><a href="/areas/new/">modifier</a> <a href="/areas/new/">supprimer</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<script>
+export default {
+  name: "ProfileView",
+  data() {
+    return {
+      user: {
+        username: "JohnDoe" // Mettez ici le nom d'utilisateur de l'utilisateur connecté
+      },
+      areas: [
+        { id: 1, name: "Voie de la Cascade", location: "Cascade"},
+        { id: 2, name: "Voie de l'Aube", location: "Montagne" },
+        { id: 3, name: "Voie du Crépuscule", location: "Forêt"}
+      ],
+      routes: [
+        { id: 1, name: "Voie de la Cascade", area: "Cascade"},
+        { id: 2, name: "Voie de l'Aube", area: "Montagne"},
+        { id: 3, name: "Voie du Crépuscule", area: "Forêt"}
+      ]
+    };
+  }
+};
+</script>
